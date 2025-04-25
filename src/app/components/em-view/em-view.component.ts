24a95bca78bd2cd20dba1470521937cabd83b1a1
eddzmaciel <eddz-maciel@hotmail.com>
2025-04-24 18:02:09 -0700

    adding codebase

--- src/app/components/em-view/em-view.component.ts
+++ src/app/components/em-view/em-view.component.ts
@@ -1,11 +1,8 @@
 import { Component, Input, OnInit, Output, ViewEncapsulation, EventEmitter, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
-//import { EventBus } from '@trutoo/event-bus';
 
-import { EventBus } from '@trutoo/event-bus';
-
-
+// ####### EVENT BUS Native-----
 // export class EventBus<DetailType = any>{
 //   private eventTarget: EventTarget;
 //   constructor(description = '') {
@@ -37,81 +34,43 @@ export class EmViewComponent implements AfterViewInit, OnInit {
   @Input('emTitle') public emTitle: string = "Title in Microfrontend";
   @Input('emDescription') public emDescription: string = "Description in Microfrontend";
   @ViewChild("emView") emView: ElementRef;
+
   //event emmiter
   @Output('emCustomEvent') emCustomEvent: EventEmitter<any> = new EventEmitter<any>();
+
   //data object
   countriesList: any[] = [];
 
-  private subs: { unsubscribe(): void }[] = [];
+  //getting data from container app
+  private _data: string;
+  @Input('containerData') set containerData(data: string) {
+    if (data) {
+      this._data = data;
+      console.log('showing data from web component!!!', this._data);
+    }
+  }
 
-  constructor(private http: HttpClient, private eventBus: EventBus) {
+  constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void {
     //console.log(this.emData);
     this.getCountries();
     //event bus
-    this.generateEventBus();
+    //this.generateEventBus();
   }
-
-  generateEventBus() {
-    this.eventBus.register('store:addToCart', { type: 'boolean' });
-    this.subs.push(eventBus.subscribe<boolean>('store:newDeals', this.onNewDeals));
-    this.onSend();
-  }
-
-  onNewDeals() {
-    /* handle new deals ... */
-    console.log('helloooooo!');
-  }
-
-  onSend() {
-    this.eventBus.publish('store:addToCart', {
-      name: 'Milk',
-      amount: '1000 ml',
-      price: 0.99,
-      organic: true,
-      stores: [
-        {
-          name: 'ACME Food AB',
-          url: 'acme-food.com'
-        }
-      ]
-    });
-  }
-
 
   ngOnDestroy() {
-    this.subs.forEach(sub => sub.unsubscribe());
+
   }
 
-
   ngAfterViewInit() {
-    //this.emView.nativeElement.emData = { token: '1-4$%65fgfgfsfsdf', userName: 'Edson Maciel' };
+    // this.emView.nativeElement.emData = { token: '1-4$%65fgfgfsfsdf', userName: 'Edson Maciel' };
     console.log(this.emView);
     console.log("client height: " + this.emView.nativeElement.clientHeight);
     console.log("client width: " + this.emView.nativeElement.clientWidth);
-    //this.generateEvent();
+
   }
-
-
-
-
-  // generateEvent() {
-  //   console.log('generating event');
-  //   const myEventBus = new EventBus('my-event-bus');
-  //   myEventBus.on('event-name', ({ detail }) => {
-  //     document.body.append(detail + ' ');
-  //   });
-  //   myEventBus.once('event-name', ({ detail }) => {
-  //     document.body.append(detail + ' ');
-  //   });
-
-
-  //   myEventBus.emit('event-name', 'hello');
-  //   myEventBus.emit('event-name', 'world');
-  // }
-
 
 
   onClick() {
